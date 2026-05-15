694b0dd13d9b942488ad40dd4b05385998124905
firepenguindisopanda <nicosmith.smith3@gmail.com>
2026-05-15 15:35:32 -0400

    feat: add barrel export for audio module

---
+++ src/lib/audio/index.ts
@@ -0,0 +1,4 @@
+export { AudioManager } from "./AudioManager";
+export { getAudioPath, getSoundNames } from "./audioConfig";
+export { useSound } from "./useSound";
+export { InteractionManager } from "./InteractionManager";
